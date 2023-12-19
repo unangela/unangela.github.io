df48a354f62d42c5d15e9d3998e814a6c5e21ab4
una <547625439@qq.com>
2023-12-19 11:27:48 +0800

    隐藏来必力广告

--- themes/next/scripts/filters/comment/livere.js
+++ themes/next/scripts/filters/comment/livere.js
@@ -12,7 +12,11 @@ hexo.extend.filter.register('theme_inject', injects => {
   injects.comment.raw('livere', `
   <div class="comments">
     <div id="lv-container" data-id="city" data-uid="{{ theme.livere_uid }}"></div>
+    <style>
+        #taboola-livere { display: none;}
+    </style>
   </div>
+
   `, {}, {cache: true});
 
   injects.bodyEnd.file('livere', path.join(hexo.theme_dir, 'layout/_third-party/comments/livere.njk'));
